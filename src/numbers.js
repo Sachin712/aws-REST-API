5231b6fa09142e0c07ccefd5d64c1b39cb0a5318
Sachin Devdhar <sachdevdhar@gmail.com>
2022-10-31 13:20:53 -0400

    numbers.js fixed

--- src/numbers.js
+++ src/numbers.js
@@ -8,9 +8,14 @@ module.exports.addNumbers = async (event) => {
   try {
     const reqBody = event.body;
     const parsedBody = JSON.parse(reqBody);
+
+    let num1 = parsedBody.num1;
+    let num2 = parsedBody.num2;
+
+    let res = num1+num2;
     return {
       statusCode: 200,
-      body: `Answer ${parsedBody?.num}`,
+      body: `Answer is ${res}`,
     };
   } catch (err) {
     return {
