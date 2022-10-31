c5230d8cab7d7f259fa6a027db7f28fa759372f1
Sachin Devdhar <sachdevdhar@gmail.com>
2022-10-31 13:16:04 -0400

    numbers.js fixed

--- src/numbers.js
+++ src/numbers.js
@@ -1,8 +1,21 @@
 "use strict";
 
 module.exports.addNumbers = async (event) => {
-  const body = JSON.parse(event.body);
-  console.log(body.num);
+  // const body = JSON.parse(event.body);
+  // console.log(body.num);
 
-  return body;
+  // return body;
+  try {
+    const reqBody = event.body;
+    const parsedBody = JSON.parse(reqBody);
+    return {
+      statusCode: 200,
+      body: `Answer ${parsedBody?.num}`,
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: "Error occured.",
+    };
+  }
 };
